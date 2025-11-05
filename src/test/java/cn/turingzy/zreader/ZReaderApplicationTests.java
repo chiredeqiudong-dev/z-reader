f516738d9bd05347eee7d1a7745fe06a5928a8d3
turingzy <turingzy@gmail.com>
2025-11-05 21:53:06 +0800

    first commit

---
+++ src/test/java/cn/turingzy/zreader/ZReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.turingzy.zreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
